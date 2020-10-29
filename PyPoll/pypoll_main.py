fb4e454c0f6e08f5bc181cc4a4635d7a29db2734
Justin Acance <jacance@Justins-MacBook-Pro.local>
2020-10-28 17:10:51 -0700

    voting count and percentages correct.

--- PyPoll/pypoll_main.py
+++ PyPoll/pypoll_main.py
@@ -10,6 +10,8 @@ import csv
 vote_index = [0, 0, 0, 0]
 candidates_list = []
 all_votes = []
+candidates = {}
+
 
 # Set path then open with csv.reader
 electioncsvpath = os.path.join("resources/election_data.csv")
@@ -27,7 +29,7 @@ with open(electioncsvpath, newline="") as electioncsv:
         votes = votes + 1
         if row[2] not in candidates_list:
             candidates_list.append(row[2])
-        all_votes.append(row[2])
+        # all_votes.append(row[2])
 
 
 
@@ -37,23 +39,24 @@ with open(electioncsvpath, newline="") as electioncsv:
     next(csvreader) # Skip header
 
     for row in csvreader:    
-        # if row[2] not in candidates_list:
-        if all_votes[0] in row[2]:
+        if candidates_list[0] == row[2]:
             vote_index[0] += 1
-        if all_votes[1] in row[2]:
+        elif candidates_list[1] == row[2]:
             vote_index[1] += 1
-        if all_votes[2] in row[2]: # Producing duplicate
+        if candidates_list[2] == row[2]: # Producing duplicate
             vote_index[2] += 1
-        if all_votes[3] in row[2]: # Producing duplicate
+        elif candidates_list[3] == row[2]: # Producing duplicate
             vote_index[3] += 1
 
 
 
 percentage = [0, 0, 0, 0]
 percentage[0] = (vote_index[0]/(votes) * 100)
-percentage[1] = vote_index[1]/(votes)
-percentage[2] = vote_index[2]/(votes)
-percentage[3] = vote_index[3]/(votes)
+percentage[1] = vote_index[1]/(votes ) * 100
+percentage[2] = vote_index[2]/(votes ) * 100
+percentage[3] = vote_index[3]/(votes) * 100
+print(all_votes)
+print(vote_index)
 
 # # Function for percentage
 # def finalpercentage(x):
@@ -66,10 +69,10 @@ print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {votes}") # The total number of votes cast
 print("-------------------------")
-print(f'{candidates_list[0]}: {vote_index[0]}, {percentage[0]}'),
-print(f'{candidates_list[1]}: {vote_index[1]}, {percentage[1]}'),
-print(f'{candidates_list[2]}: {vote_index[2]}, {percentage[2]}'), # Duplicate
-print(f'{candidates_list[3]}: {vote_index[3]}, {percentage[3]}'), # Duplicate
+print(f'{candidates_list[0]}: {vote_index[0]:,.0f}, {percentage[0]:.2f}%'),
+print(f'{candidates_list[1]}: {vote_index[1]:,.0f}, {percentage[1]:.2f}%'),
+print(f'{candidates_list[2]}: {vote_index[2]:,.0f}, {percentage[2]:.2f}%'), 
+print(f'{candidates_list[3]}: {vote_index[3]:,.0f}, {percentage[3]:.2f}%'), 
 print("-------------------------")
 print(f'Winner: {max(vote_index)}') # Needs to be name using key value pair (dictionary)
 print("-------------------------")
