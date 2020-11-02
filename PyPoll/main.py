88fe4e15e565f9d17c61b06dc58d5602432fee23
Justin Acance <jacance@Justins-MacBook-Pro.local>
2020-11-02 00:41:20 -0800

    includes output txt files

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -13,7 +13,7 @@ with open(electioncsvpath, newline="") as electioncsv:
 
     next(csvreader) # Skip header
     
-    # Set variables and start count at 0
+    # Set variables and start vote count at 0
     votes = 0
     candidates = {}
    
@@ -24,36 +24,38 @@ with open(electioncsvpath, newline="") as electioncsv:
         else:
             candidates[row[2]] += 1 # Adds a candidate vote every time a candidates name is found
 
-# Sort dictionary values/vote counts in descending order
-sorted_candidate = {k: v for k, v in sorted(candidates.items(), 
-key=lambda item: item[1], reverse=True)}
+# Sorted candidates dictionary by values/vote counts in descending order
+sorted_candidates = {
+    k: v for k, v in sorted(candidates.items(), 
+    key=lambda item: item[1], reverse=True)
+}
 
 # Convert percentage of candidate votes
 def percentfunction(parameter):
     percent = ((parameter/votes) * 100)
-    return round(percent, 2) # 
+    return round(percent, 2) 
 
 # Print results
 print("Election Results")
 print("-------------------------")
 print(f"Total Votes: {votes}") # The total number of votes cast
 print("-------------------------")
-for kv in sorted_candidate.items(): # For loop to print candidates, votes, and percentage of votes
+for kv in sorted_candidates.items(): # For loop to print candidates, votes, and percentage of votes
     print(f'{kv[0]}: {kv[1]}, {percentfunction(kv[1]):,.2f}%')
 print("-------------------------")
-print('Winner:', (list(sorted_candidate.keys())[0])) # Prints name of candidate with most votes
+print('Winner:', (list(sorted_candidates.keys())[0])) # Prints name of candidate with most votes
 print("-------------------------")
 
 # Print results to txt file
 with open("Analysis/pypoll_output.txt", "a") as f:
     print("Election Results", file=f)
     print("-------------------------", file=f)
-    print(f"Total Votes: {votes}", file=f) # The total number of votes cast
+    print(f"Total Votes: {votes}", file=f) 
     print("-------------------------", file=f)
-    for kv in sorted_candidate.items(): # For loop to print candidates, votes, and percentage of votes
+    for kv in sorted_candidates.items(): 
         print(f'{kv[0]}: {kv[1]}, {percentfunction(kv[1]):,.2f}%', file=f)
     print("-------------------------", file=f)
-    print('Winner:', (list(sorted_candidate.keys())[0]), file=f) # Prints name of candidate with most votes
+    print('Winner:', (list(sorted_candidates.keys())[0]), file=f) 
     print("-------------------------", file=f)
 
 f.close()
